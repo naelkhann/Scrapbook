535712f6d7f165550d9df5a1576f1edd68659dca
Nael Khan <naelkhann@gmail.com>
2016-01-26 15:26:13 -0500

    Added API (working) and created view in forecast page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
+//ROUTE
 weatherApp.config(function ($routeProvider) {
 		
 		$routeProvider
@@ -16,26 +17,27 @@ weatherApp.config(function ($routeProvider) {
 
 });
 
+
+//CONTROLLERS
+
 weatherApp.service('cityService', function(){
 	this.city = "New York, NY";
+})
 
-});
-
-weatherApp.controller("mainController", ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('mainController', ['$scope', 'cityService', function($scope, cityService){
 	$scope.city = cityService.city;
 
 	$scope.$watch('city', function(){
 		cityService.city = $scope.city;
-
-	});
-
+	})
 }]);
 
-weatherApp.controller("forecastController", ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
 	$scope.city = cityService.city;
 
-	$scope.weatherAPI = $resource("api.openweathermap.org/data/2.5/forecast?", { callback: "JSON_CALLBACK" }, {get: {method: "JSONP"}});
-	$scope.weatherData = $scope.weatherAPI.get({ q: $scope.city, cnt: 2});
+	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&cnt=2&appid=44db6a862fba0b067b1930da0d769e98", {callback : "JSON_CALLBACK"}, {get: {method : "JSONP"}});
+
+	$scope.weatherResult = $scope.weatherAPI.get({ q : $scope.city, cnt: 2 });
 
 }]);
 
