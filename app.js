9af565f262e853dd395dfc2251816feb4f9f8dd4
Nael Khan <naelkhann@gmail.com>
2016-01-26 01:57:45 -0500

    Added controllers, routing, ng-view

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-var weatherApp = angular.module ("weatherApp", [ngRoute, ngResource]);
+var weatherApp = angular.module ('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.config(function ($routeProvider) {
+		$routeProvider
+
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'mainController'
+		})
+
+		.when('/forecast', {
+			templateUrl: 'pages/forecast.html',
+			controller: 'forecastController'
+		})
+});
+
+weatherApp.controller("mainController", ['$scope', function($scope){
+
+}]);
+
+weatherApp.controller("forecastController", ['$scope', function($scope){
+
+}]);
+
