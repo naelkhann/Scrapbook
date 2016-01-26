e8e378abe20d0f64720f00f3ad2ad8aa280c5253
Nael Khan <naelkhann@gmail.com>
2016-01-26 03:28:44 -0500

    Added two way binding to cityService

--- app.js
+++ app.js
@@ -13,15 +13,26 @@ weatherApp.config(function ($routeProvider) {
 			templateUrl: 'pages/forecast.htm',
 			controller: 'forecastController'
 		})
-		//$locationProvider.html5Mode(true);
 
 });
 
-weatherApp.controller("mainController", ['$scope', function($scope){
+weatherApp.service('cityService', function(){
+	this.city = "New York, NY";
+
+});
+
+weatherApp.controller("mainController", ['$scope', 'cityService', function($scope, cityService){
+	$scope.city = cityService.city;
+
+	$scope.$watch('city', function(){
+		cityService.city = $scope.city;
+
+	});
 
 }]);
 
-weatherApp.controller("forecastController", ['$scope', function($scope){
+weatherApp.controller("forecastController", ['$scope', 'cityService', function($scope, cityService){
+	$scope.city = cityService.city;
 
 }]);
 
