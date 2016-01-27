05e63fbdacc2669dc73f17ece183654c8b9b6cda
Nael Khan <naelkhann@gmail.com>
2016-01-27 01:46:29 -0500

    Added ng-submit function by creating form in home.htm

--- app.js
+++ app.js
@@ -29,12 +29,16 @@ weatherApp.service('cityService', function(){
 	this.city = "New York, NY";
 })
 
-weatherApp.controller('mainController', ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('mainController', ['$scope', '$location', 'cityService', function($scope, $location cityService){
 	$scope.city = cityService.city;
 
 	$scope.$watch('city', function(){
 		cityService.city = $scope.city;
 	})
+
+	$scope.getForecast = function(){
+		$location.path("#/forecast");
+	};
 }]);
 
 weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
